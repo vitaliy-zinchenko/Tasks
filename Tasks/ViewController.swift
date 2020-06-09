ab7c8158fd8581c0c35957ac9c608e7c37c33af5
vitaliy-zinchenko <zinjvi@gmail.com>
2020-06-08 21:23:51 -0400

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Vitalii Zinchenko on 08.06.2020.
+//  Copyright © 2020 Vitalii Zinchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
